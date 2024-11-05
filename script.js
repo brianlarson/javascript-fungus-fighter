3c2083f4c371968842ff7bf1dfff5e8b79e545fd
Brian Larson <brian@brianlarson.com>
2024-11-04 23:01:41 -0600

    Get different attack types logging effects to AP and HP to console depending on type

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 
 // Set vars to track enemy hit points as well as our attack points
-let enemyHitPoints = 100;
+let hitPoints = 100;
 let attackPoints = 100;
 
-// Create function to handle attacks
+// Create function to handle attacks and handle AP and HP
 function attack(type, cost, damage) {
-  console.log(`attack function called`);
+  switch (type) {
+    case "arcane-scepter":
+      attackPoints -= 12;
+      hitPoints -= 14;
+      break;
+    case "entangle":
+      attackPoints -= 23;
+      hitPoints -= 9;
+      break;
+    case "dragon-blade":
+      attackPoints -= 38;
+      hitPoints -= 47;
+      break;
+    case "star-fire":
+      attackPoints -= 33;
+      hitPoints -= 25;
+      break;
+  }
+  console.log(`AP:`, attackPoints);
+  console.log(`HP:`, hitPoints);
 }
